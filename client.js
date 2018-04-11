50e48a8a1acd7ca39ced1ebf9f586d197e579616
nkean <nathankean2@gmail.com>
2018-04-11 10:16:19 -0500

    base mode

--- client.js
+++ client.js
@@ -9,3 +9,75 @@ const employees = [ atticus, jem, scout, robert, mayella ];
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log(employees);
+
+
+console.log( 'bonus calculator');
+
+
+let bonusBonus = 0;
+
+class Employee {
+    constructor (inputObject){
+        this.name= inputObject.name;
+        this.employeeNumber=inputObject.employeeNumber;
+        this.annualSalary=parseInt(inputObject.annualSalary);
+        this.reviewRating=inputObject.reviewRating;
+        this.bonusPercentage=this.bonusPercentage();
+        this.totalBonus=this.annualSalary*(this.bonusPercentage/100);
+        this.totalCompensation=this.totalBonus+this.annualSalary;
+
+    }
+
+    bonusPercentage(){
+        bonusBonus=0;
+        this.ratingCheck();
+        this.serviceCheck();
+        this.incomeCheck();
+        this.bonusCapper();
+        return bonusBonus;
+    }
+    ratingCheck(){
+        if (this.reviewRating <= 2){
+            bonusBonus +=0;
+        }
+        else if (this.reviewRating==3){
+            bonusBonus+=4;
+        }
+        else if (this.reviewRating==4){
+            bonusBonus+=6;
+        }
+        else if (this.reviewRating==5){
+            bonusBonus+=10;
+        }
+        return bonusBonus;
+    }
+    serviceCheck(){
+        if (this.employeeNumber.length==4){
+            bonusBonus+=5;
+        }
+        return bonusBonus;
+
+    }
+    incomeCheck(){
+        if (this.annualSalary > 65000){
+            bonusBonus-=1;
+        }
+        return bonusBonus;
+
+    }
+    bonusCapper(){
+        if (bonusBonus > 13){
+            bonusBonus=13;
+        }
+        else if (bonusBonus < 0){
+            bonusBonus=0;
+        }
+        return bonusBonus;
+
+    }
+}
+
+for (let i = 0; i < employees.length; i++) {
+    let output = new Employee(employees[i]);
+    console.log(output);
+}
